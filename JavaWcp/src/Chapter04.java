af7e40890006c5d43f7a5287e260c3038a28f7c0
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-07-20 18:48:02 +0900

    [java8]

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name;
+		name = "Mihoko";
+		System.out.println(name);
+		name = "Word";
+		System.out.println(name);
+		
+		final int num = 7;
+		System.out.println(num);
+		
+	}
+}
---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,24 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 456376535456573645L;
+		System.out.println(long1);
+		double double1 = 3.15;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+
+		Float float1 = 1.235F;
+		System.out.println(float1);
+
+		Integer integer1 = 3;
+		System.out.println(integer1);
+
+		Character c = 'あ';
+		System.out.println(c);
+
+		int[] array = { 10, 20, 30, 40, 50 };
+		System.out.println(array[3]);
+
+	}
+}
