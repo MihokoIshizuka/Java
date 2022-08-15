9b92ad38726510abef7be771a0e57a0027aa2812
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-15 18:29:58 +0900

    problem_3

--- JavaPractice/src/kita_soft/problem_2.java
+++ JavaPractice/src/kita_soft/problem_2.java
@@ -2,52 +2,52 @@ package kita_soft;
 
 public class problem_2 {
 public static void main(String[] args) {
-////	String 型の変数 s に文字列を入力し、s の値を表示するプログラムを作成しなさい。
-//	System.out.println("文字列を入力してください");
-//	String s = new java.util.Scanner(System.in).nextLine();
-//	System.out.println(s);
-//	
-////	int 型の変数 x に数値を入力し、x の値を表示するプログラムを作成しなさい。
-//	System.out.println("数字を入力してください");
-//	int a = new java.util.Scanner(System.in).nextInt();
-//	System.out.println(a);
-//	
-////	int 型の変数 x に数値を入力し、x を 1 乗、2 乗、3 乗した結果を表示するプログラムを作成しなさい。
-//	System.out.println("数字を入力してください");
-//	int b = new java.util.Scanner(System.in).nextInt();
-//	int c = 3;
-//	int ruijou = 1;
-//	for (int d = 0; d < c; d++) {
-//		ruijou *= b;
-//		System.out.println(ruijou);
-//	}
+//	String 型の変数 s に文字列を入力し、s の値を表示するプログラムを作成しなさい。
+	System.out.println("文字列を入力してください");
+	String s = new java.util.Scanner(System.in).nextLine();
+	System.out.println(s);
+	
+//	int 型の変数 x に数値を入力し、x の値を表示するプログラムを作成しなさい。
+	System.out.println("数字を入力してください");
+	int a = new java.util.Scanner(System.in).nextInt();
+	System.out.println(a);
+	
+//	int 型の変数 x に数値を入力し、x を 1 乗、2 乗、3 乗した結果を表示するプログラムを作成しなさい。
+	System.out.println("数字を入力してください");
+	int b = new java.util.Scanner(System.in).nextInt();
+	int c = 3;
+	int ruijou = 1;
+	for (int d = 0; d < c; d++) {
+		ruijou *= b;
+		System.out.println(ruijou);
+	}
 	
 //	int 型の変数 x、y にそれぞれ数値を入力し、x と y の和、差（x－y）、積、商と余り(x÷y)、を表示するプログラムを作成しなさい。
-//	System.out.println("Aの数字を入力してください");
-//	int e = new java.util.Scanner(System.in).nextInt();
-//	System.out.println("Bの数字を入力してください");
-//	int f = new java.util.Scanner(System.in).nextInt();
-//	
-//	int add = e + f;
-//	int subtract = e - f;
-//	int multiply = e * f;
-//	int division = e / f;
-//	int remainder = e % f;
-//	
-//	System.out.println("2つの数の和は " + add);
-//	System.out.println("2つの数の差は " + subtract);
-//	System.out.println("2つの数の積は " + multiply);
-//	System.out.println("2つの数の商は " + division + ", 余りは " + remainder);
-//	
+	System.out.println("Aの数字を入力してください");
+	int e = new java.util.Scanner(System.in).nextInt();
+	System.out.println("Bの数字を入力してください");
+	int f = new java.util.Scanner(System.in).nextInt();
+	
+	int add = e + f;
+	int subtract = e - f;
+	int multiply = e * f;
+	int division = e / f;
+	int remainder = e % f;
+	
+	System.out.println("2つの数の和は " + add);
+	System.out.println("2つの数の差は " + subtract);
+	System.out.println("2つの数の積は " + multiply);
+	System.out.println("2つの数の商は " + division + ", 余りは " + remainder);
+	
 //	２つの整数値を入力し、平均値を求めるプログラムを作成しなさい。
-//	System.out.println("Aの数字を入力してください");
-//	int g = new java.util.Scanner(System.in).nextInt();
-//	System.out.println("Bの数字を入力してください");
-//	int h = new java.util.Scanner(System.in).nextInt();
-//	
-//	int average = (g + h) / 2;
-//	System.out.println("この2つの数の平均値は：" + average);
-//	
+	System.out.println("Aの数字を入力してください");
+	int g = new java.util.Scanner(System.in).nextInt();
+	System.out.println("Bの数字を入力してください");
+	int h = new java.util.Scanner(System.in).nextInt();
+	
+	int average = (g + h) / 2;
+	System.out.println("この2つの数の平均値は：" + average);
+	
 //	年齢を訊ね、生まれてから現在までの、おおよその日数を表示するプログラムを作成しなさい。
 	System.out.println("年齢を入力してください");
 	int i = new java.util.Scanner(System.in).nextInt();
---
+++ JavaPractice/src/kita_soft/problem_3.java
@@ -0,0 +1,51 @@
+package kita_soft;
+
+public class problem_3 {
+public static void main(String[] args) {
+////	int 型の変数 x、y にそれぞれ数値を入力し、x が y より大きい場合に、“xはyより大きい。”という文を表示するプログラムを作成しなさい。
+//	System.out.println("Aの数字を入力してください");
+//	int a = new java.util.Scanner(System.in).nextInt();
+//	System.out.println("Bの数字を入力してください");
+//	int b = new java.util.Scanner(System.in).nextInt();
+//	
+//	if (a > b) {
+//		System.out.println("AはBよりも大きいです");
+//	}
+//	
+////	２つの整数値を入力し、大きい方（小さくない方）の数を表示するプログラムを作成しなさい。
+//	System.out.println("Aの数字を入力してください");
+//	int c = new java.util.Scanner(System.in).nextInt();
+//	System.out.println("Bの数字を入力してください");
+//	int d = new java.util.Scanner(System.in).nextInt();
+//	
+//	if (c > d) {
+//		System.out.println(c);
+//	} else if (d > c) {
+//		System.out.println(d);
+//	}
+	
+////	正の整数値を入力し、それが偶数か奇数かを判定するプログラムを作成しなさい。
+//	System.out.println("正の整数を入力してください");
+//	int e = new java.util.Scanner(System.in).nextInt();
+//	if (e % 2 == 0) {
+//		System.out.println("この数字は偶数です");
+//	} else {
+//		System.out.println("この数字は奇数です");
+//	}
+
+//	試験の点数を入力し、対応する成績を表示するプログラムを作成しなさい。
+	System.out.println("中間試験0-100点までの点数を入力してください");
+	int f = new java.util.Scanner(System.in).nextInt();
+	System.out.println("期末試験0-100点までの点数を入力してください");
+	int g = new java.util.Scanner(System.in).nextInt();
+	if (f >= 60 && g >= 60) {
+		System.out.println("合格");
+	} else if ( f + g >= 130) {
+		System.out.println("合格");
+	} else if (f + g >= 100 && (f >= 90 || g >= 90)) {
+		System.out.println("合格");
+	} else {
+		System.out.println("不合格");
+	}
+}
+}
