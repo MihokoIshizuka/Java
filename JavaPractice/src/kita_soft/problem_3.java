e585ccda939825de16cb1627aa2203f87737bdb3
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-15 18:57:46 +0900

    problem_3

--- JavaPractice/src/kita_soft/problem_3.java
+++ JavaPractice/src/kita_soft/problem_3.java
@@ -34,18 +34,34 @@ public static void main(String[] args) {
 //	}
 
 //	試験の点数を入力し、対応する成績を表示するプログラムを作成しなさい。
-	System.out.println("中間試験0-100点までの点数を入力してください");
-	int f = new java.util.Scanner(System.in).nextInt();
-	System.out.println("期末試験0-100点までの点数を入力してください");
-	int g = new java.util.Scanner(System.in).nextInt();
-	if (f >= 60 && g >= 60) {
-		System.out.println("合格");
-	} else if ( f + g >= 130) {
-		System.out.println("合格");
-	} else if (f + g >= 100 && (f >= 90 || g >= 90)) {
-		System.out.println("合格");
+//	System.out.println("中間試験0-100点までの点数を入力してください");
+//	int f = new java.util.Scanner(System.in).nextInt();
+//	System.out.println("期末試験0-100点までの点数を入力してください");
+//	int g = new java.util.Scanner(System.in).nextInt();
+//	if (f >= 60 && g >= 60) {
+//		System.out.println("合格");
+//	} else if ( f + g >= 130) {
+//		System.out.println("合格");
+//	} else if (f + g >= 100 && (f >= 90 || g >= 90)) {
+//		System.out.println("合格");
+//	} else {
+//		System.out.println("不合格");
+//	}
+	
+//	曜日と、午前、午後、夜間の区別を入力し、病院が開いているか、休診であるかを表示するプログラムを作成しなさい
+	System.out.println("曜日を数字で選択してください（0=日曜、1=月曜、2=火曜、3=水曜、4=木曜、5=金曜、6=土曜）");
+	int h = new java.util.Scanner(System.in).nextInt();
+	System.out.println("時間帯を数字で選択してください（0=午前、1=午後、2=夜間）");
+	int i = new java.util.Scanner(System.in).nextInt();
+	
+	if (h == 1) { 
+		System.out.println("休診");
+	} else if ((h == 2 && i == 0) || (h == 3 && i == 2) || (h == 5 && i == 0)) {
+		System.out.println("休診");
+	} else if (h == 6 && (i == 1 || i == 2)) {
+		System.out.println("休診");
 	} else {
-		System.out.println("不合格");
+		System.out.println("診療しています");
 	}
 }
 }
