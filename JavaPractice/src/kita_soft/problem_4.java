a65d66ce1913b9ad8a2706532fe36df63fe286b9
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-16 19:17:18 +0900

    problem_4

---
+++ JavaPractice/src/kita_soft/problem_4.java
@@ -0,0 +1,42 @@
+package kita_soft;
+
+public class problem_4 {
+public static void main(String[] args) {
+	
+//	“SPAM”という単語を 10 回表示するプログラムを作成しなさい。
+//	String spam = "SPAM";	
+//	for (int i = 0; i < 10; i++) {
+//		System.out.println(spam);
+//	}
+	
+//	九九、三の段（ 3 ～ 27 の 3 の倍数）を表示するプログラムを作成しなさい。
+//	int num = 3;
+//	for (int a = 1; a < 10; a++) {
+//		int result = num * a;
+//		System.out.println(result);
+//	}
+	
+//	2 の 1 乗から 8 乗までを計算し表示するプログラムを作成しなさい。
+//	int b = 1;
+//	for (int c = 1; c < 9; c++) {
+//		b *= 2;
+//		System.out.println(b);
+//		
+//	}
+	
+//	7 の階乗を計算し、表示するプログラムを作成しなさい
+//	int c = 1;
+//	for (int d = 1; d <= 7; d++) {
+//		c *= d;
+//	}
+//	System.out.println(c);
+	
+//	整数を 10 回入力し、平均値を求めるプログラムを作成しなさい
+	int sum = 0;
+	for (int d = 0; d < 10; d++) {
+		int e = new java.util.Scanner(System.in).nextInt();
+		sum =+ e;
+		System.out.println(sum / 10);
+	}
+}
+}
