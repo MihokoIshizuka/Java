6b6ae713855538da5fb359a03034a26b3ae8139d
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-19 12:00:11 +0900

    kita-soft

--- JavaPractice/src/kita_soft/problem4_17_22.java
+++ JavaPractice/src/kita_soft/problem4_17_22.java
@@ -5,50 +5,50 @@ import java.util.Scanner;
 public class problem4_17_22 {
 public static void main(String[] args) {
 //	九九表（一の段～九の段）を表示するプログラムを作成しなさい。
-//	for (int a = 1; a <= 9; a++) {
-//		for (int b = 1; b <= 9; b++) {
-//			System.out.printf("%2d",a * b);
-//			if (b == 9) {
-//				System.out.println();
-//			}
-//		}
-//	}
+	for (int a = 1; a <= 9; a++) {
+		for (int b = 1; b <= 9; b++) {
+			System.out.printf("%2d",a * b);
+			if (b == 9) {
+				System.out.println();
+			}
+		}
+	}
 	
 //	数値を繰り返して入力し、0 が入力されたら入力を止め、それまでの合計を表示するプログラムを作成しなさい
-//	Scanner scan = new Scanner(System.in);
-//	int num = 1;
-//	while (true) {
-//		num = scan.nextInt();
-//		if (num == 0)
-//			break;
-//	}
-//	System.out.println("終了");
+	Scanner scan = new Scanner(System.in);
+	int num = 1;
+	while (true) {
+		num = scan.nextInt();
+		if (num == 0)
+			break;
+	}
+	System.out.println("終了");
 	
 //	数値を繰り返して入力し、0 が入力されたら入力を止め、平均値を表示するプログラムを作成しなさい。
-//	Scanner scn = new Scanner(System.in);
-//	int sum = 0;
-//	int count = 0;
-//	while (true) {
-//		int number = scn.nextInt();
-//		if (number != 0) {
-//			sum += number;
-//			count++;
-//		} else {
-//			break;
-//		}
-//	}
-//	System.out.println("終了");
-//	System.out.println(sum / count);
+	Scanner scn = new Scanner(System.in);
+	int sum = 0;
+	int count = 0;
+	while (true) {
+		int number = scn.nextInt();
+		if (number != 0) {
+			sum += number;
+			count++;
+		} else {
+			break;
+		}
+	}
+	System.out.println("終了");
+	System.out.println(sum / count);
 	
 //	サイズを示す数値を入力し、何等かの文字で例のような三角形を表示するプログラムを作成しなさい。
-//	Scanner s = new Scanner(System.in);
-//	int size = s.nextInt();
-//		for (int c = 1; c <= size; c++) {
-//			for (int d = 1; d <= c; d++) {
-//				System.out.print("$");
-//			}
-//			System.out.println();
-//		}
+	Scanner s = new Scanner(System.in);
+	int size = s.nextInt();
+		for (int c = 1; c <= size; c++) {
+			for (int d = 1; d <= c; d++) {
+				System.out.print("$");
+			}
+			System.out.println();
+		}
 	
 //	サイズを示す数値を入力し、そのサイズの×印を何等かの文字で表示するプログラムを作成しなさい。
 	Scanner sc = new Scanner(System.in);
--- JavaPractice/src/test.java
+++ JavaPractice/src/test.java
@@ -1,5 +1,6 @@
 
 public class test {
+//	ALH
 public static void main(String[] args) {
 	for (int a = 1; a <= 3; a++) {
 		for (int b = 3; b >= 1; b--) {
