200564bd9b28e161da76604453041d4191188fc7
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-22 23:36:50 +0900

    problem5_1

---
+++ JavaPractice/src/kita_soft/problem5_1.java
@@ -0,0 +1,13 @@
+package kita_soft;
+
+import java.util.Scanner;
+
+public class problem5_1 {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	for (int i = 0; i < 10; i++) {
+		int num = sc.nextInt();
+		System.out.println(num * 2);
+	}
+}
+}
