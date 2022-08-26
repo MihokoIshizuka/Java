c0680c6acbe5c135e7c4a9f4b10dcc02b33285fa
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-26 23:48:25 +0900

    prob5_1

--- JavaPractice/src/sevenDays_5/prob5_1.java
+++ JavaPractice/src/sevenDays_5/prob5_1.java
@@ -2,7 +2,9 @@ package sevenDays_5;
 
 public class prob5_1 {
 public static void main(String[] args) {
-	int[] a = new int[7];
-	
+	for (int i = 1; i <= 7; i++) {
+		int a = new java.util.Random().nextInt(10)+1;
+		System.out.print("a[" + i + "] =" + a + " ");
+	}
 }
 }
