ed37d2c1060cecca21f6910acc4d713a633488f3
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-30 21:38:55 +0900

    prob5_2

---
+++ JavaPractice/src/sevenDays_5/prob5_2.java
@@ -0,0 +1,14 @@
+package sevenDays_5;
+
+public class prob5_2 {
+	public static void main(String[] args) {
+		double[] a = new double[4];
+		
+		a[0] = 0.2;
+		a[1] = -5.1;
+		a[2] = 3.2;
+		a[3] = 1.8;
+		
+	System.out.println("a[0]=" + a[0] + ", a[1]=" + a[1] + ", a[2]=" + a[2] + ", a[3]=" + a[3]);
+	}
+}
