d386c0697e6c8fecb9aba3e4401cf38ec84e76ed
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-31 23:10:35 +0900

    prob5_5

--- JavaPractice/src/sevenDays_5/prob5_5.java
+++ JavaPractice/src/sevenDays_5/prob5_5.java
@@ -2,6 +2,26 @@ package sevenDays_5;
 
 public class prob5_5 {
 	public static void main(String[] args) {
+		int data[] = new int[10];
+		for (int i = 0; i < data.length; i++) {
+			data[i] = new java.util.Random().nextInt(10)+1;
+			System.out.print(data[i] + " ");
+		}
+		System.out.println();
 		
+		System.out.print("3の倍数：");
+		for (int i = 0; i < data.length; i++) {
+			if(data[i] % 3 == 0) {
+				System.out.print(data[i] + " ");
+			}
+		}
+		System.out.println();
+		
+		System.out.print("3の倍数以外の数：");
+		for (int i = 0; i < data.length; i++) {
+			if(data[i] % 3 != 0) {
+				System.out.print(data[i] + " ");
+			}
+		}
 	}
 }
