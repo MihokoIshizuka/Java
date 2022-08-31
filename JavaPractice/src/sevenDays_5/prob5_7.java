c3b20b50e64c3fe95c9e70520310943500f038a7
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-31 23:49:06 +0900

    prob5_7

--- JavaPractice/src/sevenDays_5/prob5_7.java
+++ JavaPractice/src/sevenDays_5/prob5_7.java
@@ -10,6 +10,7 @@ public class prob5_7 {
 		}
 		System.out.println();
 		
+//		配列の値の合計値と、平均値を表示する。
 		System.out.print("合計値：");
 		for (int i = 0; i < num.length; i++) {
 			math += num[i];
@@ -20,7 +21,8 @@ public class prob5_7 {
 		System.out.print("平均値：");
 		System.out.print(math / num.length);
 		System.out.println();
-		
+
+//		平均値よりも大きい数を表示する
 		System.out.print("平均値より大きい数：");
 		for (int i = 0; i < num.length; i++) {
 			if (num[i] > math / num.length) {
@@ -29,6 +31,7 @@ public class prob5_7 {
 		}
 		System.out.println();
 		
+//		平均値よりも小さい数を表示する。		
 		System.out.print("平均値より小さい数：");
 		for (int i = 0; i < num.length; i++) {
 			if (num[i] < math / num.length) {
