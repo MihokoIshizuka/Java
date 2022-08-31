dd9e3744324a9177e280331747f1fe8b161f6812
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-31 23:47:59 +0900

    prob5_7

---
+++ JavaPractice/src/sevenDays_5/prob5_7.java
@@ -0,0 +1,39 @@
+package sevenDays_5;
+
+public class prob5_7 {
+	public static void main(String[] args) {
+		int num[] = new int[5];
+		int math = 0;
+		for (int i = 0; i < num.length; i++) {
+			num[i] = new java.util.Random().nextInt(10)+1;
+			System.out.print(num[i] + " ");
+		}
+		System.out.println();
+		
+		System.out.print("合計値：");
+		for (int i = 0; i < num.length; i++) {
+			math += num[i];
+		}
+		System.out.print(math);
+		System.out.println();
+		
+		System.out.print("平均値：");
+		System.out.print(math / num.length);
+		System.out.println();
+		
+		System.out.print("平均値より大きい数：");
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] > math / num.length) {
+				System.out.print(num[i] + " ");
+			}
+		}
+		System.out.println();
+		
+		System.out.print("平均値より小さい数：");
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] < math / num.length) {
+				System.out.print(num[i] + " ");
+			}
+		}
+	}
+}
