8e8a59c98df57c71f65836bcf30dc17e6ed24e1f
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-05 22:47:43 +0900

    sample601

---
+++ JavaPractice/src/sevenDays_6/Sample601.java
@@ -0,0 +1,16 @@
+package sevenDays_6;
+
+public class Sample601 {
+	public static void main(String[] args) {
+//		インスタンスの作成
+		SampleClass01 s = new SampleClass01();
+//		フィールドへの値の代入
+		s.n = 100;
+		s.s = "Hello";
+		int ans = s.add(1, 2);
+		String str = s.add("World");
+		System.out.println(ans);
+		System.out.println(str);
+		s.showNum();
+	}
+}
---
+++ JavaPractice/src/sevenDays_6/SampleClass01.java
@@ -0,0 +1,17 @@
+package sevenDays_6;
+
+public class SampleClass01 {
+	int n = 10;
+	String s = "field";
+	
+	int add(int a, int b){
+		return a + b;
+	}
+	String add(String s) {
+		return this.s + s;
+	}
+	void showNum() {
+		System.out.println("n = " + n);
+	}
+	
+}
