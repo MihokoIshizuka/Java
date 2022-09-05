6f3562eda458eb40b0de196060492f00ded64b64
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-05 23:21:22 +0900

    prob_2

---
+++ JavaPractice/src/sevenDays_6/Calc.java
@@ -0,0 +1,20 @@
+package sevenDays_6;
+
+public class Calc {
+	//  加算
+    double add(double a,double b){
+        return a + b;
+    }
+    //  引き算
+    double sub(double a,double b){
+        return a - b;
+    }
+    //  掛け算
+    double mul(double a,double b){
+    	return a * b;
+    }
+//    割り算
+    double div(double a,double b) {
+    	return a / b;
+    }
+}
---
+++ JavaPractice/src/sevenDays_6/prob6_2.java
@@ -0,0 +1,12 @@
+package sevenDays_6;
+
+public class prob6_2 {
+	public static void main(String[] args) {
+        Calc c = new Calc();
+        double a = 4.0,b = 2.0;
+        System.out.println(a + " + " + b + " = " + c.add(a,b));
+        System.out.println(a + " - " + b + " = " + c.sub(a,b));
+        System.out.println(a + " * " + b + " = " + c.mul(a,b));
+        System.out.println(a + " / " + b + " = " + c.div(a,b));
+    }
+}
