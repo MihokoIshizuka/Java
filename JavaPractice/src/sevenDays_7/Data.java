33ff575db00e8e4c3aaa123654d84b543ad4155a
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-06 15:37:24 +0900

    prob7_1

---
+++ JavaPractice/src/sevenDays_7/Data.java
@@ -0,0 +1,8 @@
+package sevenDays_7;
+
+public class Data {
+	 //  メンバ変数number
+    private int number = 0;
+    //  メンバ変数comment
+    private String comment = "";
+}
---
+++ JavaPractice/src/sevenDays_7/prob7_1.java
@@ -0,0 +1,11 @@
+package sevenDays_7;
+
+public class prob7_1 {
+	public static void main(String args[]){
+        Data d = new Data();
+        d.setNumber(100);
+        d.setComment("Programming Java");
+        System.out.println( "number = " + d.getNumber());
+        System.out.println( "comment = " + d.getComment());
+    }
+}
