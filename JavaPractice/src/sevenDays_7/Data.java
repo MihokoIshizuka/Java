ebe7982a8c816be0b2d6be4b8ef5b93e5ed4bdb7
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-09 23:30:20 +0900

    Data

--- JavaPractice/src/sevenDays_7/Data.java
+++ JavaPractice/src/sevenDays_7/Data.java
@@ -1,8 +1,28 @@
 package sevenDays_7;
 
 public class Data {
-	 //  メンバ変数number
+	public Data() {
+		System.out.println("コンストラクタ");
+	}
+	//  メンバ変数number
     private int number = 0;
     //  メンバ変数comment
     private String comment = "";
+    
+    
+    public void setComment(String comment) {
+    	this.comment = comment;
+    }
+    
+    public void setNumber(int number) {
+    	this.number = number;
+    }
+    
+    public int getNumber() {
+    	return this.number;
+    }
+    
+    public String getComment() {
+    	return this.comment;
+    }
 }
