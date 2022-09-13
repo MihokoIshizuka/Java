5e9cef790ccae4f16e55d334259183976e1caa8b
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-13 23:16:05 +0900

    10行の出力

---
+++ JavaPractice/src/sevenDays_7/paiza_enshu3.java
@@ -0,0 +1,11 @@
+package sevenDays_7;
+
+public class paiza_enshu3 {
+//	10行の出力
+	public static void main(String[] args) {
+        int[] array = {813, 1, 2, 923874, 23648, 782356, 3256, 2342, 24324, 112};
+        for (int value : array) {
+            System.out.println(value);
+        }
+    }
+}
