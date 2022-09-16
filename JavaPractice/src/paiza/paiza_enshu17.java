15deea79d47322b70d0cfe26a2109dd453f2f1b7
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-16 21:51:25 +0900

    ドット絵

---
+++ JavaPractice/src/paiza/paiza_enshu17.java
@@ -0,0 +1,29 @@
+package paiza;
+
+public class paiza_enshu17 {
+//	ドット絵
+	public static void main(String[] args) {
+
+        int[][] enemyImage =
+            {{0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+             {0,0,0,1,0,1,0,0,0,0,1,1,1,0,0,0},
+             {0,0,1,0,0,0,0,1,1,1,1,1,1,1,0,0},
+             {1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+             {1,0,0,1,1,1,0,0,0,0,1,1,1,0,0,1},
+             {1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1},
+             {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0}};
+             
+        for (int[] line : enemyImage) {
+            for (int dot : line) {
+                if (dot == 1) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
