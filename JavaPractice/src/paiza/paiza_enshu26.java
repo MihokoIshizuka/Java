48c043da14b56cd00da20a470fec3e424b69ff91
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-20 17:23:11 +0900

    paiza_enshu26

---
+++ JavaPractice/src/paiza/paiza_enshu26.java
@@ -0,0 +1,12 @@
+package paiza;
+
+public class paiza_enshu26 {
+//	累乗の計算
+	public static void main(String[] args) {
+	       int A = 202;
+	       int B = 134; 
+	       int C = 107;
+	       int D = (A + B) * C;
+	       System.out.println((int)(Math.pow(D, 2)));
+	    }
+}
