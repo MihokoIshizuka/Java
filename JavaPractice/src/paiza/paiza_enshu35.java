3a0542f7ccf6a9090625056b5749d9e561164517
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-26 13:39:28 +0900

    paiza_enshu35

---
+++ JavaPractice/src/paiza/paiza_enshu35.java
@@ -0,0 +1,18 @@
+package paiza;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class paiza_enshu35 {
+	    public static void main(String[] args) {
+	        HashMap<String, String> enemyMap = new HashMap<String, String>();
+	        enemyMap.put("ザコ", "スライム");
+	        enemyMap.put("中ボス", "ドラゴン");
+	        enemyMap.put("ラスボス", "魔王");
+	        
+	        for (Entry<String, String> entry : enemyMap.entrySet()){
+	            System.out.print(entry.getKey() + ", ");
+	            System.out.println(entry.getValue());
+	        }
+	 
+	    }
+}
